287dd541e0c5f99fc2ac430f727d0acf47cb1350
sergei_vydra <vydrasergei@gmail.com>
2020-05-06 13:56:38 +0300

    added main page logic and static data

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from movies.models import (Category, Movie, Actor, Genre,  MovieShot, Review, RatingStar, Rating)
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Movie)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(MovieShot)
+admin.site.register(Review)
+admin.site.register(RatingStar)
+admin.site.register(Rating)
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from movies.models import Movie
 
-# Create your views here.
+
+class MoviesView(View):
+    """List of movies."""
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movies.html", {"movie_list": movies})
+
+
+class MovieDetailView(View):
+    """Movie Detail View."""
+
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk)
+        return render(request, "movies/movie_detail.html", {"movie": movie})
