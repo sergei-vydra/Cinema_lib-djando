2cb101e316e8429e9d8c5cf4b695488d0a8c2f53
sergei_vydra <vydrasergei@gmail.com>
2022-06-28 10:49:03 +0300

    git_action test 4

--- cinema_lib/asgi.py
+++ cinema_lib/asgi.py
@@ -3,6 +3,7 @@ ASGI config for cinema_lib project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
+
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
